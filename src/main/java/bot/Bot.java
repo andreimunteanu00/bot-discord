7902be53b107f3cea83b8800b83f746457b13544
andrei.munteanu <Aloko2013!>
2022-02-27 20:33:39 +0200

    fix: warning heroku cli

--- src/main/java/bot/Bot.java
+++ src/main/java/bot/Bot.java
@@ -5,6 +5,7 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import org.apache.log4j.BasicConfigurator;
 
 import javax.security.auth.login.LoginException;
 import java.util.EnumSet;
@@ -12,7 +13,7 @@ import java.util.EnumSet;
 public class Bot {
 
     public static void main(String[] args) throws LoginException {
-
+        BasicConfigurator.configure();
         JDA bot = JDABuilder
                 .createDefault(System.getenv("TOKEN"),
                 GatewayIntent.GUILD_MEMBERS,
